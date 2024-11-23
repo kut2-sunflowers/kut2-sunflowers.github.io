213ed2fe82d7b8df3e50901f3ff82b198bfe1b4d
duckiendoan <kiendd.hrt@gmail.com>
2024-11-23 18:04:58 +0700

    add files

---
+++ _next/static/chunks/app/page-8aeb2cf7af9ed593.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{72153:(e,s,r)=>{Promise.resolve().then(r.bind(r,49531)),Promise.resolve().then(r.bind(r,71364))},49531:(e,s,r)=>{"use strict";r.d(s,{Divider:()=>i.Z,Image:()=>a.Z,Space:()=>n.Z});var i=r(14110),a=r(76055),n=r(23288)}},e=>{var s=s=>e(e.s=s);e.O(0,[733,7,978,130,215,744],()=>s(72153)),_N_E=e.O()}]);
